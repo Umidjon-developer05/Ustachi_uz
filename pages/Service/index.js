127bdc0a35c19a347860dbf05506b5fdc21dd1d7
Umidjon <127615615+123456789098765432111@users.noreply.github.com>
2024-03-03 19:18:57 +0500

    asd

--- pages/Service/index.js
+++ pages/Service/index.js
@@ -1,33 +1,11 @@
 import React from "react";
 import Card from "./_components/Card";
-import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 const Service = () => {
   return (
-<<<<<<< HEAD
     <div className="mt-24">
       <Card />
-=======
-    <div
-    className='mt-24'
-    >
- 
-      <Card/>
-
-      
->>>>>>> origin/master
     </div>
   );
 };
-export async function getStaticProps({ locale }) {
-  return {
-    props: {
-      ...(await serverSideTranslations(locale, ["Service"])),
-    },
-  };
-}
-<<<<<<< HEAD
-export default Service;
-=======
 
-export default Service
->>>>>>> origin/master
+export default Service;
