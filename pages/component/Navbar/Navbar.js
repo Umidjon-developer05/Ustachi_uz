899b742f02553ea477cbae119de58490523b6e43
Umidjon <127615615+Umidjon-developer05@users.noreply.github.com>
2024-03-07 23:36:46 +0500

    Update Navbar.js

--- pages/component/Navbar/Navbar.js
+++ pages/component/Navbar/Navbar.js
@@ -153,7 +153,7 @@ const Navbar = () => {
                 <Link href={"/sign-in"}>
                   <Button
                     type="button"
-                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none p-2 focus:ring-blue-300 font-medium rounded-lg text-sm  text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none  focus:ring-blue-300 font-medium rounded-lg text-sm  text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                   >
                     Get started
                   </Button>
